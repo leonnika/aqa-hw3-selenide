0f2814e1267e6bb9ea6cd52a679f37eb9ca8b3c1
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-06-02 00:34:27 +0500

    added test

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -1,25 +1,59 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideTest {
-    @Test
-    void shouldSubmitRequest()
-    {
-        String expected;
 
-        open("http://localhost:9999");
+   @BeforeAll
+    static void setUpAll() {
+       open("http://localhost:9999");
+   }
+
+    @Nested
+    public class NameValidPhoneValidCheckIs {
+        private String expected;
+
+        @BeforeEach
+        void init() {
             $("[data-test-id=phone] input").setValue("+79270000000");
             $("[data-test-id=agreement]").click();
             expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        }
+        @Test
+    void shouldSentFormAllValid()
+    {
             $("[data-test-id=name] input").setValue("Иванов Василий");
             $("[role='button']").click();
             $("[data-test-id=order-success]").shouldHave(exactText(expected));
     }
 
-}
+        @Test
+        void shouldSentFormDableName() {
+            $("[data-test-id=name] input").setValue("Иванов Петр Иван");
+            $("[role='button']").click();
+            $("[data-test-id=order-success]").shouldHave(exactText(expected));
+        }
+
+        void shouldSentFormDableNameSeparator() {
+            $("[data-test-id=name] input").setValue("Иванов Петр-Иван");
+            $("[role='button']").click();
+            $("[data-test-id=order-success]").shouldHave(exactText(expected));
+        }
+
+        @Test
+        void shouldSentFormDableNameLong() {
+            $("[data-test-id=name] input").setValue("Иванова-Перевозкина-Пограничникова Александра Виктория Валентина");
+            $("[role='button']").click();
+            $("[data-test-id=order-success]").shouldHave(exactText(expected));
+        }
+
+}}
