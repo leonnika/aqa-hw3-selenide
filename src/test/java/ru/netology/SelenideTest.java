faa172d3a0bbb34f676ab3e204e61ed7a075de8b
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-06-02 00:41:15 +0500

    added test

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -1,40 +1,34 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideTest {
-
-   @BeforeAll
-    static void setUpAll() {
-       open("http://localhost:9999");
-   }
+    private String expected;
 
     @Nested
     public class NameValidPhoneValidCheckIs {
-        private String expected;
 
         @BeforeEach
         void init() {
+            open("http://localhost:9999");
             $("[data-test-id=phone] input").setValue("+79270000000");
             $("[data-test-id=agreement]").click();
             expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         }
+
         @Test
-    void shouldSentFormAllValid()
-    {
+        void shouldSentFormAllValid() {
             $("[data-test-id=name] input").setValue("Иванов Василий");
             $("[role='button']").click();
             $("[data-test-id=order-success]").shouldHave(exactText(expected));
-    }
+        }
 
         @Test
         void shouldSentFormDableName() {
@@ -56,4 +50,5 @@ public class SelenideTest {
             $("[data-test-id=order-success]").shouldHave(exactText(expected));
         }
 
-}}
+    }
+}
