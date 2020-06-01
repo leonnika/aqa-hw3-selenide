7d92eb43dd00b7311c2177f83a41601c13831f70
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-06-02 01:34:31 +0500

    added SUCCESSFUL test

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -1,6 +1,6 @@
 package ru.netology;
 
-import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,12 +8,13 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SelenideTest {
     private String expected;
 
     @Nested
-    public class NameValidPhoneValidCheckIs {
+    public class NameValidOptionsPhoneValidCheckIs {
 
         @BeforeEach
         void init() {
@@ -37,6 +38,7 @@ public class SelenideTest {
             $("[data-test-id=order-success]").shouldHave(exactText(expected));
         }
 
+        @Test
         void shouldSentFormDableNameSeparator() {
             $("[data-test-id=name] input").setValue("Иванов Петр-Иван");
             $("[role='button']").click();
@@ -49,6 +51,120 @@ public class SelenideTest {
             $("[role='button']").click();
             $("[data-test-id=order-success]").shouldHave(exactText(expected));
         }
+    }
 
+    @Nested
+    public class NameNotValidOptionsPhoneValidCheckIs {
+
+        @BeforeEach
+        void init() {
+            open("http://localhost:9999");
+            $("[data-test-id=phone] input").setValue("+79270000000");
+            $("[data-test-id=agreement]").click();
+            expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        }
+
+        @Test
+        void shouldNotSentFormNameEnglish() {
+            $("[data-test-id=name] input").setValue("Ivanov Ivan");
+            $("[role='button']").click();
+            $("[class='input__sub']").shouldHave(exactText(expected));
+        }
+
+        @Test
+        void shouldNotSentFormNameNumber() {
+            $("[data-test-id=name] input").setValue("88888");
+            $("[role='button']").click();
+            $("[class='input__sub']").shouldHave(exactText(expected));
+        }
+
+        @Test
+        void shouldNotSentFormNameEmpty() {
+            $("[role='button']").click();
+            $("[class='input__sub']").shouldHave(exactText("Поле обязательно для заполнения"));
+        }
+
+        @Test
+        void shouldNotSentFormNameSpecialSymbols() {
+            $("[data-test-id=name] input").setValue("@#$$%^&(*&(");
+            $("[role='button']").click();
+            $("[class='input__sub']").shouldHave(exactText(expected));
+        }
+// Тысты которые будут fail
+//        @Test
+//        void shouldNotSentFormNameOnly() {
+//         $("[data-test-id=name] input").setValue("Иванов");
+//            $("[role='button']").click();
+//            $("[class='input__sub']").shouldHave(exactText(expected));
+//        }
+
+        //     @Test
+//        void shouldNotSentFormNameDifferentCaseLetters() {
+//           ("[data-test-id=name] input").setValue("иВаноВ иВАН");
+//            $("[role='button']").click();
+//            $("[class='input__sub']").shouldHave(exactText(expected));
+//        }
+    }
+
+    @Nested
+    public class PhoneNotValidNameValidCheckIs {
+        private String expected;
+
+        @BeforeEach
+        void init() {
+            open("http://localhost:9999");
+            $("[data-test-id=name] input").setValue("Иванов Василий");
+            $("[data-test-id=agreement]").click();
+            expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        }
+
+        @Test
+        void shouldNotSentFormPhoneSpecialSymbols() {
+            $("[data-test-id=phone] input").setValue(";%;%%:%");
+            $("[role='button']").click();
+            SelenideElement phone = $("[data-test-id=phone]");
+            phone.$("[class='input__sub']").shouldHave(exactText(expected));
+        }
+
+        @Test
+        void shouldNotSentFormPhoneEmpty() {
+            $("[role='button']").click();
+            SelenideElement phone = $("[data-test-id=phone]");
+            phone.$("[class='input__sub']").shouldHave(exactText("Поле обязательно для заполнения"));
+        }
+
+        @Test
+        void shouldNotSentFormPhoneFistSymbolNotPlus() {
+            $("[data-test-id=phone] input").setValue("89898989890");
+            $("[role='button']").click();
+            SelenideElement phone = $("[data-test-id=phone]");
+            phone.$("[class='input__sub']").shouldHave(exactText(expected));
+        }
+
+        @Test
+        void shouldNotSentFormPhoneNot11element() {
+            $("[data-test-id=phone] input").setValue("89898");
+            $("[role='button']").click();
+            SelenideElement phone = $("[data-test-id=phone]");
+            phone.$("[class='input__sub']").shouldHave(exactText(expected));
+        }
+
+// Тысты которые будут fail
+//        @Test
+//        void shouldNotSentFormPhoneFist0(){
+//        $("[data-test-id=phone] input").setValue("+08989898909");
+//        $("[role='button']").click();
+//        SelenideElement phone =$("[data-test-id=phone]");
+//        phone.$("[class='input__sub']").shouldHave(exactText(expected));
+//        }
+    }
+
+    @Test
+    void CheckIsNotNamePhoneValid() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Иванов Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[role='button']").click();
+        assertEquals("rgba(255, 92, 92, 1)", $("[data-test-id=agreement]").getCssValue("color"));
     }
 }
